02a443d871b598dbe60fad066e015ce4fbb4f378
MichaelSlakdov <msladkov2002@gmail.com>
2024-11-06 14:34:24 +0300

    feat: added indicator logic

--- frontend/src/components/student_indicator/student_indicator.jsx
+++ frontend/src/components/student_indicator/student_indicator.jsx
@@ -1,18 +1,43 @@
-function StudentIndicator() {
+function StudentIndicator({ val, setVal }) {
+  function increaseVal() {
+    if (val < 2) {
+      setVal(val + 1);
+    }
+  }
+
+  function decreaseVal() {
+    if (val > 0) {
+      setVal(val - 1);
+    }
+  }
+
+  function getColor() {
+    if (val === 0) return "red";
+    if (val === 1) return "yellow";
+    if (val === 2) return "green";
+  }
+
   return (
     <div className="studentIndicator">
       <link rel="stylesheet" href="student_indicator.css" />
-      <button className="studentIndicatorBtn">+</button>
+      <button className="studentIndicatorBtn" onClick={increaseVal}>
+        +
+      </button>
       <div className="studentIndicatorDisplay">
-        <div className="studentIndicatorDisplayElement" id="displayHigh"></div>
-        <div
-          className="studentIndicatorDisplayElement"
-          id="displayMedium"
-        ></div>
-        <div className="studentIndicatorDisplayElement" id="displayLow"></div>
+        <div className="studentIndicatorDisplayElement" id="displayHigh">
+          2
+        </div>
+        <div className="studentIndicatorDisplayElement" id="displayMedium">
+          1
+        </div>
+        <div className="studentIndicatorDisplayElement" id="displayLow">
+          0
+        </div>
       </div>
       <div className="studentIndicatorDisplayElement" id="displayLow"></div>
-      <button className="studentIndicatorBtn">-</button>
+      <button className="studentIndicatorBtn" onClick={decreaseVal}>
+        -
+      </button>
     </div>
   );
 }
--- frontend/src/components/student_main/student_main.jsx
+++ frontend/src/components/student_main/student_main.jsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import StudentIndicator from "../student_indicator/student_indicator";
 
 function StudentMain() {
+  const [vibe, setVibe] = useState(1);
+  const [pon, setPon] = useState(1);
   return (
     <div className="studentMainPage">
       <div className="indicatorsContainer">
         <div className="indicatorContainer">
           пон
-          <StudentIndicator />
+          <StudentIndicator val={pon} setVal={setPon} />
         </div>
         <div className="indicatorContainer">
           вайб
-          <StudentIndicator />
+          <StudentIndicator val={vibe} setVal={setVibe} />
         </div>
       </div>
       <form>
         <input type="submit" value="ЖЁСТКИЙ НЕПОН" />
       </form>
+      <div>
+        vibe: {vibe}
+        pon: {pon}
+      </div>
     </div>
   );
 }
