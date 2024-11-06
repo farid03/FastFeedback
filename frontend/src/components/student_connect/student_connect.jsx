9f589191b349e85d62ce89178d4f5468d6fc9900
MichaelSlakdov <msladkov2002@gmail.com>
2024-11-06 13:39:31 +0300

    feat: added student connect page template

---
+++ frontend/src/components/student_connect/student_connect.jsx
@@ -0,0 +1,12 @@
+function StudentConnect () {
+    return (
+        <div className="studentConnectPage">
+            <form>
+                <input type="text" title="ID Лекции"></input>
+                <input type="submit" value="Подключиться"></input>
+            </form>
+        </div>
+    )
+}
+
+export default StudentConnect;
