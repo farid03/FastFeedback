92db62152eaa84b692481788d621abfb2afc1c6c
MichaelSlakdov <msladkov2002@gmail.com>
2024-11-06 13:11:41 +0300

    feat: role dispatch page template

---
+++ frontend/src/components/role_dispatch/role_dispatch.jsx
@@ -0,0 +1,11 @@
+function RoleDispatch() {
+  return (
+    <div className="roleDispatchPage">
+      <header className="pageHeader">Выберите роль</header>
+      <button className="roleDispatchBtn"> Студент </button>
+      <button className="roleDispatchBtn"> Преподаватель </button>
+    </div>
+  );
+}
+
+export default RoleDispatch;
