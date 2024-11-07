dcbe14dadc3f6a4265daa3251943a5a6972ef729
e.kolesenkova <e.kolesenkova@tinkoff.ru>
2024-11-07 14:04:58 +0300

    Добавлен редирект запросов

---
+++ backend/src/main/kotlin/com/feedback/fast/controller/RedirectController.kt
@@ -0,0 +1,13 @@
+package com.feedback.fast.controller
+
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class RedirectController {
+    @RequestMapping("/**")
+    fun redirectToHome(): String {
+        return "redirect:"
+    }
+}
