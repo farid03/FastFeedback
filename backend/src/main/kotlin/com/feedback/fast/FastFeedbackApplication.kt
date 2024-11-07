526e0e9befe3febef066317c4e678ec4362bb996
f.a.kurbanov <f.a.kurbanov@tinkoff.ru>
2024-11-07 19:07:42 +0300

    fix

--- backend/src/main/kotlin/com/feedback/fast/FastFeedbackApplication.kt
+++ backend/src/main/kotlin/com/feedback/fast/FastFeedbackApplication.kt
@@ -3,10 +3,8 @@ package com.feedback.fast
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableScheduling
-@EnableWebMvc
 @SpringBootApplication
 class FastFeedbackApplication
 
