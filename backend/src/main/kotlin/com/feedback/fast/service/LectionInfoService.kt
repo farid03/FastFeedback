27c087ab7f7f1e3a6a1e0778c0f1e9ed24b318d1
selfofly <Vladislav.Boyko@tune-it.ru>
2024-11-08 09:52:29 +0300

    Tried fixing button x2

--- backend/src/main/kotlin/com/feedback/fast/service/LectionInfoService.kt
+++ backend/src/main/kotlin/com/feedback/fast/service/LectionInfoService.kt
@@ -49,8 +49,9 @@ class LectionInfoService(
         val userStats = userLectionStats.getOrPut(uuid) { mutableMapOf() }
 
         userStats[token] = StatResponseDto(vibeLevel = vibeLevel, ponLevel = ponLevel)
-
+        val isNeeded = lectionsWithStatLevels[uuid].isNeedSoundNotification
         lectionsWithStatLevels[uuid] = calculateAverageStats(userStats.values)
+        lectionsWithStatLevels[uuid].isNeedSoundNotification = isNeeded
     }
 
     fun pressButton(uuid: String) {
